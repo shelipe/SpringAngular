848331b8135c380304b6cb4b40a5fe132ce51040
Felipe <fmc1825@gmail.com>
2019-10-30 10:49:45 -0300

    Primeros Componentes Angular
    Se crean los componentes para parte I Grafica (Listar,Add,Editar)

--- SpringAngular/src/app/app-routing.module.ts
+++ SpringAngular/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { AddComponent } from './Persona/add/add.component';
+import { EditarComponent } from './Persona/editar/editar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'listar', component:ListarComponent},
+  {path:'add', component:AddComponent},
+  {path:'Editar', component:EditarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
